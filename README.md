6344a72c089b0ab411913aca46830c1f937ba5a7
Lucio Murua <luciomurua1992@gmail.com>
2022-10-13 10:00:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tp9-LandingPageCompleta
+ Completed landing page selling a Css course with many sections using HTML, Css and JS.
---
+++ js/slider.js
@@ -0,0 +1,13 @@
+(function(){
+    const sliders = [...document.querySelectorAll('.testimony__body')];
+    const buttonNext = document.querySelector('#next');
+    const buttonBefore = document.querySelector('#before');
+    let value;
+    buttonNext.addEventListener('click', ()=>{
+        changePosition(1);
+    })
+    buttonBefore.addEventListener('click', ()=>{
+        changePosition(-1);
+    })
+
+})()
